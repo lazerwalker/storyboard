5308bb379fdc2855000d6167df33eaf3f8989e41
Mike Walker <michael@lazerwalker.com>
2015-12-26 18:44:53 -0800

    Add stubs for nodeGraph unit tests

--- tests/nodeGraphTests.js
+++ tests/nodeGraphTests.js
@@ -10,4 +10,40 @@ import * as Actions from '../src/gameActions'
 
 var graph;
 
-// TODO: Unit-test this.
+describe("completePassage", function() {
+  context("when it is the last passage in the node", function() {
+    it("should transition to the next node", function() {
+
+    });
+  });
+
+  context("when it isn't the last passage", function() {
+    it("should transition to the next passage", function() {
+
+    });
+  });
+});
+
+describe("checkChoiceTransitions", function() {
+  context("when the node isn't complete", function() {
+    it("should do nothing, even if a choice is ready", function() {
+
+    });
+  });
+
+  context("when the node is complete", function() {
+    it("should change nodes when the conditions are met", function() {
+
+    });
+
+    it("should not change nodes when the conditions aren't met", function() {
+
+    });
+  });
+});
+
+describe("playCurrentPassage", function() {
+  it("should output with the correct passage", function() {
+
+  });
+});
