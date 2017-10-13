9cfc7c868eae2a12b170b535c3fcd9f00ed267f8
Mike Lazer-Walker <michael@lazerwalker.com>
2017-10-13 12:13:10 -0700

    Set up proper TS compilation pipeline, and run tests via ts-node

--- tests/nodeBagTests.ts
+++ tests/nodeBagTests.ts
@@ -1,48 +1,34 @@
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai'
 
+chai.use(sinonChai);
+const expect = chai.expect
+
 import { Bag } from '../src/nodeBag'
 import * as Actions from '../src/gameActions'
 import * as Parser from 'storyboard-lang'
 import { State } from '../src/state'
-import { Game } from '../src/game'
-
-
-chai.use(sinonChai);
-const expect = chai.expect
 
 describe("filtering nodes", function() {
   context("when some nodes match the predicate but others don't", function() {
     it("should only return the nodes that match", function() {
-      const story = `
-        ## first
-          [foo <= 10]
-          track: default
-
-        ## second
-          [foo <= 9]
-          track: default
-
-        ## third
-          [foo <= 0]
-          track: default
-      `
-
-      const game = new Game(story)
-      const bag = game.story.bag
-      console.log(JSON.stringify(bag.nodes, null, 3))
+      const bagData: Parser.NodeBag = {
+        first: {nodeId: "first", predicate: { "foo": { "lte": 10 }}, track: "default"},
+        second: {nodeId: "second", predicate: { "foo": { "lte": 9 }}, track: "default"},
+        third: {nodeId: "third", predicate: { "foo": { "lte": 0 }}, track: "default"}
+      }
 
       let action, data;
 
-      bag.dispatch! = (passedAction: string, passedData: any) => {
+      function dispatch(passedAction: string, passedData: any) {
         expect(passedAction).to.equal(Actions.TRIGGERED_BAG_NODES)
 
         let firstNode = bag.nodes["first"]
         let secondNode = bag.nodes["second"]
-        console.log(passedData)
         expect(passedData).to.eql({"default": [firstNode, secondNode]});
       }
 
+      const bag = new Bag(bagData, dispatch);
       const state = new State()
       state.foo = "5"
 
